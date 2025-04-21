d2fed5c3b4eba41725f6149b2499a5c6a27f87df
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-04-20 22:50:49 -0400

    lc 706

--- 706.design-hash-map.java
+++ 706.design-hash-map.java
@@ -5,22 +5,78 @@
  */
 
 // @lc code=start
+class Pair<U, V> {
+    public U first;
+    public V second;
+
+    public Pair(U first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Bucket {
+    private List<Pair<Integer, Integer>> bucket;
+
+    public Bucket() {
+        this.bucket = new LinkedList<Pair<Integer, Integer>>();
+    }
+
+    public Integer get(Integer key) {
+        for (Pair<Integer, Integer> pair : this.bucket) {
+            if (pair.first.equals(key)) {
+                return pair.second;
+            }
+        }
+        return -1;
+    }
+
+    public void update(Integer key, Integer value) {
+        boolean found = false;
+        for (Pair<Integer, Integer> pair : this.bucket) {
+            if (pair.first.equals(key)) {
+                pair.second = value;
+                found = true;
+            }
+        }
+        if (!found == true) this.bucket.add(new Pair(key, value));
+    }
+
+    public void remove(Integer key) {
+        for (Pair<Integer, Integer> pair : this.bucket) {
+            if (pair.first.equals(key)) {
+                this.bucket.remove(pair);
+                break;
+            }
+        }
+    }
+}
+
 class MyHashMap {
+    private int key_space;
+    private List<Bucket> hash_table;
 
     public MyHashMap() {
-        
+        key_space = 2069;
+        hash_table = new ArrayList();
+        for (int i = 0; i < key_space; i++) {
+            hash_table.add(new Bucket());
+        }
     }
     
     public void put(int key, int value) {
-        
+        int index = key % key_space;
+        hash_table.get(index).update(key, value);
     }
     
     public int get(int key) {
-        
+        int index = key % key_space;
+        return hash_table.get(index).get(key);
     }
     
     public void remove(int key) {
-        
+        int index = key % key_space;
+        hash_table.get(index).remove(key);
     }
 }
 
