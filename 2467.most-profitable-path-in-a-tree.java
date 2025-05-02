227e86545f24dfa1815effc743bd524d9a7086c5
Ryan <wx225@cornell.edu>
2025-05-02 17:23:02 -0400

    lc 2467 modified

--- 2467.most-profitable-path-in-a-tree.java
+++ 2467.most-profitable-path-in-a-tree.java
@@ -6,45 +6,40 @@
 
 // @lc code=start
 class Solution {
-    int n;
-    List<List<Integer>> tree;
-    int[] distanceFromBob;
+    Map<Integer, List<Integer>> neighs;
+    Map<Integer, Integer> distToBob;
 
     public int mostProfitablePath(int[][] edges, int bob, int[] amount) {
-        n = amount.length;
-        tree = new ArrayList();
-        for (int i = 0; i < n; i++) {
-            tree.add(new ArrayList());
-        }
-
+        neighs = new HashMap();
+        distToBob = new HashMap();
+        distToBob.put(bob, 0);
+        for (int i = 0; i < amount.length; i++) neighs.put(i, new ArrayList());
         for (int i = 0; i < edges.length; i++) {
-            tree.get(edges[i][0]).add(edges[i][1]);
-            tree.get(edges[i][1]).add(edges[i][0]);
+            neighs.get(edges[i][0]).add(edges[i][1]);
+            neighs.get(edges[i][1]).add(edges[i][0]);
         }
-
-        distanceFromBob = new int[n];
-
-        return findPaths(0, 0, 0, bob, amount);
+        int ans = helper(0, 0, 0, bob, amount);
+        return ans;
     }
-    
-    public int findPaths(int sourceNode, int parentNode, int time, int bob, int[] amount) {
+
+    public int helper(int node, int parent, int time, int bob, int[] amount) {
         int maxChild = Integer.MIN_VALUE;
-        int maxIncome = 0;
-        distanceFromBob[sourceNode] = sourceNode == bob ? 0 : n;
-        
-        for (int adj : tree.get(sourceNode)) {
-            if (adj != parentNode) {
-                maxChild = Math.max(maxChild, findPaths(adj, sourceNode, time + 1, bob, amount));
-                distanceFromBob[sourceNode] = Math.min(distanceFromBob[sourceNode], distanceFromBob[adj] + 1);
+        int maxProfit = 0;
+        int distFromBob = amount.length;
+        for (int i = 0; i < neighs.get(node).size(); i++) {
+            int n = neighs.get(node).get(i);
+            if (n != parent) {
+                int nn = helper(n, node, time + 1, bob, amount);
+                maxChild = Math.max(maxChild, nn);
+                distFromBob = Math.min(distFromBob, distToBob.get(n) + 1);
             }
         }
-
-        if (distanceFromBob[sourceNode] > time) {
-            maxIncome += amount[sourceNode];
-        } else if (distanceFromBob[sourceNode] == time) {
-            maxIncome += amount[sourceNode] / 2;
+        if (node != bob) {
+            distToBob.put(node, distFromBob);
         }
-        return maxChild == Integer.MIN_VALUE ? maxIncome : maxIncome + maxChild;
+        if (distToBob.get(node) == time) maxProfit += amount[node] / 2;
+        else if (time < distToBob.get(node)) maxProfit += amount[node];
+        return maxChild == Integer.MIN_VALUE ? maxProfit : maxProfit + maxChild;
     }
 }
 // @lc code=end
