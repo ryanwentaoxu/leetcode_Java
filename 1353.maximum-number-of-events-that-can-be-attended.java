3d69ea1a32355660df98507e5c43b8406fa7d7ea
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-05-03 16:07:54 -0400

    lc 1353 modified

--- 1353.maximum-number-of-events-that-can-be-attended.java
+++ 1353.maximum-number-of-events-that-can-be-attended.java
@@ -6,26 +6,30 @@
 
 // @lc code=start
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 class Solution {
     public int maxEvents(int[][] events) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int ans = 0;
-        Comparator<int[]> c = new Comparator<int[]>(){
+        Comparator<int[]> c = new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) return o1[1] - o2[1];
                 return o1[0] - o2[0];
             }
         };
         Arrays.sort(events, c);
-        int i = 0;
-        for (int d = 1; d <= 100000; d++) {
-            while (i < events.length && events[i][0] == d) {
-                pq.add(events[i][1]);
-                i++;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int start = 1;
+        int end = 100000;
+        int count = 0;
+        int ans = 0;
+        for (int day = start; day <= end; day++) {    
+            while (count < events.length && events[count][0] == day) {
+                pq.add(events[count][1]);
+                count += 1;
             }
-            while (pq.isEmpty() != true && pq.peek() < d) {
+            while (pq.isEmpty() == false && pq.peek() < day) {
                 pq.poll();
             }
             if (pq.isEmpty() == false) {
