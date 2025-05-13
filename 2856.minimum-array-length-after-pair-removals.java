4fb2684382c3f27cd98bda92631da4e9c66ddf9f
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-05-12 21:02:48 -0400

    lc 2856

--- 2856.minimum-array-length-after-pair-removals.java
+++ 2856.minimum-array-length-after-pair-removals.java
@@ -7,22 +7,22 @@
 // @lc code=start
 class Solution {
     public int minLengthAfterRemovals(List<Integer> nums) {
-        Map<Integer, Integer> freq = new HashMap();
-        for (int i : nums) {
-            freq.put(i, freq.getOrDefault(i, 0) + 1);
+        Map<Integer, Integer> map = new HashMap();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) pq.add(entry.getValue());
-        while (pq.size() > 1) {
-            int num1 = pq.poll();
-            int num2 = pq.poll();
-            num1 --;
-            num2 --;
-            if (num1 > 0) pq.offer(num1);
-            if (num2 > 0) pq.offer(num2);
+        PriorityQueue<Integer> freq = new PriorityQueue<Integer>(Collections.reverseOrder());
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            freq.offer(entry.getValue());
         }
         int ans = 0;
-        while (pq.size() > 0) ans += pq.poll();
+        while (!freq.isEmpty() && freq.size() >= 2) {
+            int num1 = freq.poll();
+            int num2 = freq.poll();
+            if (num1 > 1) freq.offer(num1 - 1);
+            if (num2 > 1) freq.offer(num2 - 1);
+        }
+        while (!freq.isEmpty()) ans += freq.poll();
         return ans;
     }
 }
