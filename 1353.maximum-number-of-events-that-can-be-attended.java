e34e3fa843058da509389f221d66fd24170b8181
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-05-16 01:10:29 -0400

    lc 1353 modified

--- 1353.maximum-number-of-events-that-can-be-attended.java
+++ 1353.maximum-number-of-events-that-can-be-attended.java
@@ -7,6 +7,7 @@
 // @lc code=start
 class Solution {
     public int maxEvents(int[][] events) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         Comparator<int[]> c = new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
@@ -14,19 +15,19 @@ class Solution {
             }
         };
         Arrays.sort(events, c);
-        PriorityQueue<Integer> pq = new PriorityQueue();
-        int i = 0;
-        int ans = 0;
+        int index = 0;
+        int ans = 0; 
         for (int d = 1; d <= 100000; d++) {
-            while (i < events.length && events[i][0] == d) {
-                pq.offer(events[i++][1]);
+            while (index < events.length && events[index][0] == d) {
+                pq.offer(events[index][1]);
+                index++;
             }
             while (!pq.isEmpty() && pq.peek() < d) {
                 pq.poll();
             }
             if (!pq.isEmpty()) {
                 pq.poll();
-                ans++;
+                ans += 1;
             }
         }
         return ans;
