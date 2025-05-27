190a47cfeae6d086a09641b343844a3976a3dbae
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-05-27 12:09:36 -0400

    lc 2918

---
+++ 221.maximal-square.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=221 lang=java
+ *
+ * [221] Maximal Square
+ */
+
+// @lc code=start
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        
+    }
+}
+// @lc code=end
+
---
+++ 2918.minimum-equal-sum-of-two-arrays-after-replacing-zeros.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2918 lang=java
+ *
+ * [2918] Minimum Equal Sum of Two Arrays After Replacing Zeros
+ */
+
+// @lc code=start
+class Solution {
+    public long minSum(int[] nums1, int[] nums2) {
+        
+    }
+}
+// @lc code=end
+
