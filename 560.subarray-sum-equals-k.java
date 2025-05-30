3ded060ee5098d26f1bcd317ff17879a618e8c1c
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-05-30 03:24:38 -0400

    lc560

---
+++ 560.subarray-sum-equals-k.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=560 lang=java
+ *
+ * [560] Subarray Sum Equals K
+ */
+
+// @lc code=start
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap();
+        map.put(0, 1);
+        int sum = 0;
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                ans += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
