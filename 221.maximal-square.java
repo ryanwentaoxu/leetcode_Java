70badac9dd1bfc1943b3f2cf4f2790ac77dc73b5
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-05-30 15:39:49 -0400

    lc 221

--- 221.maximal-square.java
+++ 221.maximal-square.java
@@ -7,7 +7,20 @@
 // @lc code=start
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int ans = 0;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '0') {
+                    continue;
+                }
+                dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        return ans * ans;
     }
 }
 // @lc code=end
