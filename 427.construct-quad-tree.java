9ef1c6d716e08f354a3997f0547911e001faf862
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-05-30 14:20:47 -0400

    lc 427

--- 427.construct-quad-tree.java
+++ 427.construct-quad-tree.java
@@ -85,36 +85,73 @@ class Node {
 }
 */
 
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
 class Solution {
-    // Returns true if all the values in the matrix are the same; otherwise, false.
-    boolean sameValue(int[][] grid, int x1, int y1, int length) {
-        for (int i = x1; i < x1 + length; i++) {
-            for (int j = y1; j < y1 + length; j++)
-                if (grid[i][j] != grid[x1][y1])
-                    return false;
+    public boolean sameValue(int x, int y, int length, int[][] grid) {
+        int std = grid[x][y];
+        for (int i = x ; i < x + length; i++) {
+            for (int j = y; j < y + length; j++) {
+                if (grid[i][j] != std) return false;
+            }
         }
         return true;
     }
 
-    Node solve(int[][] grid, int x1, int y1, int length) {
-        // Return a leaf node if all values are the same.
-        if (sameValue(grid, x1, y1, length)) {
-            return new Node(grid[x1][y1] == 1, true);
-        } else {
-            Node root = new Node(false, false);
-
-            // Recursive call for the four sub-matrices.
-            root.topLeft = solve(grid, x1, y1, length / 2);
-            root.topRight = solve(grid, x1, y1 + length / 2, length / 2);
-            root.bottomLeft = solve(grid, x1 + length / 2, y1, length / 2);
-            root.bottomRight = solve(grid, x1 + length / 2, y1 + length / 2, length / 2);
-
-            return root;
+    public Node helper(int[][] grid, int x, int y, int length) {
+        if (sameValue(x, y, length, grid)) {
+            boolean val = grid[x][y] == 1 ? true : false;
+            return new Node(val, true);
         }
+        Node root = new Node(false, false);
+        root.topLeft = helper(grid, x, y, length / 2);
+        root.topRight = helper(grid, x, y + length / 2, length / 2);
+        root.bottomLeft = helper(grid, x + length / 2, y, length / 2);
+        root.bottomRight = helper(grid, x + length / 2, y + length / 2, length / 2);
+        return root;
     }
-
+    
     public Node construct(int[][] grid) {
-        return solve(grid, 0, 0, grid.length);
+        int n = grid.length;
+        return helper(grid, 0, 0, n);
     }
 }
 // @lc code=end
