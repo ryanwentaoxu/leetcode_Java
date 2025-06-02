cfa63461d6568b46d38368bc2beea3d5e3ebf992
Ryan <wentaoxu@seas.upenn.edu>
2025-06-02 16:13:25 -0400

    lc 253

--- 253.meeting-rooms-ii.java
+++ 253.meeting-rooms-ii.java
@@ -5,44 +5,32 @@
  */
 
 // @lc code=start
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Comparator<int[]> c = new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[0] - o2[0] == 0 ? o1[1] - o2[1] : o1[0] - o2[0];
-            } 
-        };
-        Arrays.sort(intervals, c); 
-
-        int ans = 1;
-        List<List<int[]>> l = new ArrayList();
-        l.add(new ArrayList<>());
-        l.get(0).add(intervals[0]);
-        for (int i = 1; i < intervals.length; i++) {
-            int[] t = intervals[i];
-            int start = t[0];
-            int end = t[1];
-            boolean isInsert = false;
-            for (int j = 0; j < l.size(); j++) {
-                if (start >= l.get(j).get(l.get(j).size() - 1)[1]) {
-                    isInsert = true;
-                    l.get(j).add(t);
-                    break;
+        Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        List<List<int[]>> rooms = new ArrayList();
+        for (int i = 0; i < intervals.length; i++) {
+            int[] current = intervals[i];
+            if (rooms.size() == 0) {
+                rooms.add(new ArrayList());
+                rooms.get(0).add(current);
+            } else {
+                boolean added = false;
+                for (int j = 0; j < rooms.size(); j++) {
+                    int[] prev = rooms.get(j).get(rooms.get(j).size() - 1);
+                    if (prev[1] <= current[0]) {
+                        added = true;
+                        rooms.get(j).add(current);
+                        break;
+                    }
+                }
+                if (!added) {
+                    rooms.add(new ArrayList());
+                    rooms.get(rooms.size() - 1).add(current);   
                 }
             }
-            if (isInsert == false) {
-                l.add(new ArrayList<>());
-                l.get(l.size() - 1).add(t);
-            }
         }
-        return l.size();
+        return rooms.size();
     }
 }
 // @lc code=end
