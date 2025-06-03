e67e41358e1fa38d279da191c368ee33049a54f6
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-06-03 18:19:17 -0400

    lc 777

--- 777.swap-adjacent-in-lr-string.java
+++ 777.swap-adjacent-in-lr-string.java
@@ -6,23 +6,24 @@
 
 // @lc code=start
 class Solution {
-    public boolean canTransform(String start, String end) {
-        int n = start.length();
+    public boolean canTransform(String start, String result) {
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (start.charAt(i) == 'X') count ++;
-            if (end.charAt(i) == 'X') count --;
+        for (int i = 0; i < start.length(); i++) {
+            if (start.charAt(i) == 'X') count += 1;
+            if (result.charAt(i) == 'X') count -= 1;
         }
         if (count != 0) return false;
         int i = 0;
         int j = 0;
-        while (i < n && j < n) {
-            while (i < n && start.charAt(i) == 'X') i++;
-            while (j < n && end.charAt(j) == 'X') j++;
-            if (i == n || j == n) {
-                return i == n && j == n;
+        while (i < start.length() && j < result.length()) {
+            while (i < start.length() && start.charAt(i) == 'X') {
+                i++;
             }
-            if (start.charAt(i) != end.charAt(j)) return false;
+            while (j < result.length() && result.charAt(j) == 'X') {
+                j++;
+            }
+            if (i == start.length() || j == result.length()) return i == j;
+            if (start.charAt(i) != result.charAt(j)) return false;
             if (start.charAt(i) == 'L' && i < j) return false;
             if (start.charAt(i) == 'R' && i > j) return false;
             i++;
