6cd0aa0be487162adcec6da32054eb527d131f37
Ryan <wentaoxu@seas.upenn.edu>
2025-06-05 16:20:12 -0400

    lc 36

--- 36.valid-sudoku.java
+++ 36.valid-sudoku.java
@@ -10,37 +10,27 @@ class Solution {
         int N = 9;
         int[][] rows = new int[N][N];
         int[][] cols = new int[N][N];
-        int[][] boxes = new int[N][N];
+        int[][] cells = new int[N][N];
+        for (int[] r : rows) Arrays.fill(r, -1);
+        for (int[] c : cols) Arrays.fill(c, -1);
+        for (int[] c : cells) Arrays.fill(c, -1);
 
-        for (int r = 0; r < N; r++) {
-            for (int c = 0; c < N; c++) {
-                if (board[r][c] == '.') {
-                    continue;
-                }
-                int pos = board[r][c] - '1';
-                
-                if (rows[r][pos] == 1) {
-                    return false;
-                }
-
-                rows[r][pos] = 1;
-
-                if (cols[c][pos] == 1) {
-                    return false;
-                }
-
-                cols[c][pos] = 1;
-
-                int idx = (r / 3) * 3 + c / 3;
-                if (boxes[idx][pos] == 1) {
-                    return false;
-                }
-
-                boxes[idx][pos] = 1;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == '.') continue;
+                int pos = (int)(board[i][j] - '0');
+                pos -= 1;
+                int rowIndex = i;
+                int colIndex = j;
+                int cellIndex = (i / 3) * 3 + j / 3;
+                if (rows[rowIndex][pos] != -1) return false;
+                if (cols[colIndex][pos] != -1) return false;
+                if (cells[cellIndex][pos] != -1) return false;
+                rows[rowIndex][pos] = 1;
+                cols[colIndex][pos] = 1;
+                cells[cellIndex][pos] = 1;
             }
         }
         return true;
     }
 }
-// @lc code=end
-
