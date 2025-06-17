30f0262c23ed54bcf8cc6828422a7a6a5ba55178
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-06-16 23:56:25 -0400

    lc 704

---
+++ 704.binary-search.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=704 lang=java
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
