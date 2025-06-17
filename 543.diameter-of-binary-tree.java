d5ed56bd8385f1ed918e7d3a4564318e8f515b82
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-06-17 00:13:23 -0400

    lc 543

---
+++ 543.diameter-of-binary-tree.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=543 lang=java
+ *
+ * [543] Diameter of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans;
+    public int helper(TreeNode node) {
+        if (node == null) return 0;
+        int left = helper(node.left);
+        int right = helper(node.right);
+        ans = Math.max(ans, left + right + 2);
+        return Math.max(left + 1, right + 1);
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = Integer.MIN_VALUE;
+        helper(root);
+        return ans - 2;
+    }
+}
+// @lc code=end
+
