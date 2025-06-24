c0e7dc37d94a7e7938de3aae50e8129ccb60d595
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-06-24 15:04:27 -0400

    lc 15

--- 15.3-sum.java
+++ 15.3-sum.java
@@ -6,36 +6,34 @@
 
 // @lc code=start
 class Solution {
-    List<List<Integer>> ans;
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        ans = new ArrayList();
-        for (int i = 0; i < nums.length - 2; i++) {
+        List<List<Integer>> ans = new ArrayList();
+        for (int i = 0; i < nums.length; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
-            helper(nums, i);
+            helper(nums, nums[i], i + 1, ans);
         }
         return ans;
     }
 
-    public void helper(int[] nums, int index) {
-        int left = index + 1;
+    public void helper(int[] nums, int n, int start, List<List<Integer>> ans) {
+        int left = start;
         int right = nums.length - 1;
-        while (left < right){
-            int s = nums[index] + nums[left] + nums[right];
-            if (s == 0) {
+        while (left < right) {
+            if (nums[left] + nums[right] + n == 0) {
                 List<Integer> tmp = new ArrayList();
-                tmp.add(nums[index]);
+                tmp.add(n);
                 tmp.add(nums[left]);
                 tmp.add(nums[right]);
                 ans.add(tmp);
-                while (left < right && nums[left] == nums[left + 1]) {
+                while (nums[left] == nums[left + 1] & left + 1 < right) {
                     left += 1;
                 }
                 left += 1;
-            } else if (s > 0) {
-                right -= 1;
-            } else {
+            } else if (nums[left] + nums[right] + n < 0) {
                 left += 1;
+            } else {
+                right -= 1;
             }
         }
     }
