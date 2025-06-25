4cd9cf6e92f40a1ccdece843067a4476bf0623e8
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-06-25 13:05:04 -0400

    lc 224

--- 224.basic-calculator.java
+++ 224.basic-calculator.java
@@ -7,38 +7,36 @@
 // @lc code=start
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> operand = new Stack();
-        Stack<Integer> num = new Stack();
-        int n = 0;
-        int ans = 0;
+        Stack<Integer> nums = new Stack();
+        Stack<Integer> op = new Stack();
         int sign = 1;
-
+        int num = 0;
+        int ans = 0;
         for (int i = 0; i < s.length(); i++) {
             if (Character.isDigit(s.charAt(i))) {
-                n = n * 10 + (int)(s.charAt(i) - '0');
+                num = num * 10 + (int)(s.charAt(i) - '0');
             } else if (s.charAt(i) == '+') {
-                ans += n * sign;
-                n = 0;
+                ans += num * sign;
+                num = 0;
                 sign = 1;
             } else if (s.charAt(i) == '-') {
-                ans += n * sign;
-                n = 0;
+                ans += num * sign;
+                num = 0;
                 sign = -1;
             } else if (s.charAt(i) == '(') {
-                num.push(ans);
-                operand.push(sign);
+                nums.push(ans);
+                op.push(sign);
                 ans = 0;
-                n = 0;
+                num = 0;
                 sign = 1;
             } else if (s.charAt(i) == ')') {
-                ans += sign * n;
-                ans = num.pop() + operand.pop() * ans;
-                n = 0;
+                ans += num * sign;
+                ans = nums.pop() + op.pop() * ans;
+                num = 0;
                 sign = 1;
             }
         }
-
-        return ans + n * sign;
+        return ans + sign * num;
     }
 }
 // @lc code=end
