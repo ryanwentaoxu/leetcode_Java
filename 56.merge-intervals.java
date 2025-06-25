68ff4de823394da275afa32279c7a60f80c0d412
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-06-25 11:39:43 -0400

    lc 56

--- 56.merge-intervals.java
+++ 56.merge-intervals.java
@@ -5,35 +5,32 @@
  */
 
 // @lc code=start
-
-import java.util.List;
-
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Comparator<int[]> c = new Comparator<int[]>() {
+        List<int[]> list = new ArrayList();
+        Comparator<int[]> c = new Comparator<int[]>(){
             @Override
             public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) return o1[1] - o2[1];
                 return o1[0] - o2[0];
             }
         };
         Arrays.sort(intervals, c);
-        List<int[]> ret = new ArrayList();
+        List<int[]> l = new ArrayList();
         for (int i = 0; i < intervals.length; i++) {
-            if (i == 0) ret.add(intervals[i]);
-            else {
-                if (intervals[i][0] <= ret.get(ret.size() - 1)[1]) {
-                    int start = ret.get(ret.size() - 1)[0];
-                    int end = Math.max(intervals[i][1], ret.get(ret.size() - 1)[1]);
-                    ret.set(ret.size() - 1, new int[]{start, end});
-                } else {
-                    ret.add(intervals[i]);
+            if (i == 0) {
+                l.add(intervals[i]);
+            } else {
+                if (intervals[i][0] > l.get(l.size() - 1)[1]) l.add(intervals[i]);
+                else {
+                    int[] toAdd = new int[]{l.get(l.size() - 1)[0], Math.max(intervals[i][1], l.get(l.size() - 1)[1])};
+                    l.set(l.size() - 1, toAdd);
                 }
             }
         }
-        int[][] ans = new int[ret.size()][2];
-        int count = 0;
-        for (int[] t : ret) {
-            ans[count++] = t;
+        int[][] ans = new int[l.size()][2];
+        for (int i = 0; i < l.size(); i++) {
+            ans[i] = l.get(i);
         }
         return ans;
     }
