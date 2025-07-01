93ce584178c725f1ae22f193d02e9095026fb345
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-07-01 17:29:05 -0400

    lc 349

---
+++ 349.intersection-of-two-arrays.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=349 lang=java
+ *
+ * [349] Intersection of Two Arrays
+ */
+
+// @lc code=start
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> seen = new HashMap();
+        List<Integer> result = new ArrayList();
+
+        for (int x : nums1) {
+            seen.put(x, 1);
+        }
+
+        for (int x : nums2) {
+            if (seen.containsKey(x) && seen.get(x) == 1) {
+                result.add(x);
+                seen.put(x, 0);
+            }
+        }
+        return result.stream().mapToInt(i->i).toArray();
+    }
+}
+// @lc code=end
+
