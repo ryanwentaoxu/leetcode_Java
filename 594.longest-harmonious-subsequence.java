d06fbbda889da43632cdf292b8034018a60b1b63
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-07-08 02:59:57 -0400

    lc 594

---
+++ 594.longest-harmonious-subsequence.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=594 lang=java
+ *
+ * [594] Longest Harmonious Subsequence
+ */
+
+// @lc code=start
+class Solution {
+    public int findLHS(int[] nums) {
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        int res = 0;
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.containsKey(num + 1))
+                res = Math.max(res, map.get(num) + map.get(num + 1));
+            if (map.containsKey(num - 1))
+                res = Math.max(res, map.get(num) + map.get(num - 1));
+        }
+        return res;
+    }
+}
+// @lc code=end
+
