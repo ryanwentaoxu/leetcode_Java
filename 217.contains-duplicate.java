cf6cc6953420792872af9499d68878743a947c2e
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-07-08 02:41:20 -0400

    lc 217

---
+++ 217.contains-duplicate.java
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=217 lang=java
+ *
+ * [217] Contains Duplicate
+ */
+
+// @lc code=start
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+    Set<Integer> set = new HashSet<>(nums.length);
+    for (int x: nums) {
+        if (set.contains(x)) return true;
+        set.add(x);
+    }
+    return false;
+}
+}
+// @lc code=end
+
