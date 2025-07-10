3a443c603705d4c08806efd0eb344e5689f5ef07
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-07-10 19:16:16 -0400

    lc 523

---
+++ 523.continuous-subarray-sum.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=523 lang=java
+ *
+ * [523] Continuous Subarray Sum
+ */
+
+// @lc code=start
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int prefixMod = 0;
+        Map<Integer, Integer> modSeen = new HashMap();
+        modSeen.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            prefixMod = (prefixMod + nums[i]) % k;
+            if (modSeen.containsKey(prefixMod)) {
+                if (i - modSeen.get(prefixMod) > 1) return true;
+            }
+            else modSeen.put(prefixMod, i);
+        }
+        return false;
+    }
+}
+// @lc code=end
+
