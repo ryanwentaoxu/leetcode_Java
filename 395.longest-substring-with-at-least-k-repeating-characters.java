6c06d0a96c6ada831fc78b774eba4e0f3a635f83
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-07-12 15:30:24 -0400

    lc 395

---
+++ 395.longest-substring-with-at-least-k-repeating-characters.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=395 lang=java
+ *
+ * [395] Longest Substring with At Least K Repeating Characters
+ */
+
+// @lc code=start
+class Solution {
+    public int longestSubstring(String s, int k) {
+        
+    }
+}
+// @lc code=end
+
