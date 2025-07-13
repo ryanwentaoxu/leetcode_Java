d1c273365d7aabf7d0d4c72943bb0e177687d05d
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-07-12 23:19:57 -0400

    lc 131

---
+++ 131.palindrome-partitioning.java
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=131 lang=java
+ *
+ * [131] Palindrome Partitioning
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<String>> partition(String s) {
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        List<List<String>> result = new ArrayList();
+        dfs(result, s, 0, new ArrayList<String>(), dp);
+        return result;
+    }
+
+    void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
+        if (start >= s.length()) result.add(new ArrayList(currentList));
+        for (int end = start; end < s.length(); end++) {
+            if (s.charAt(end) == s.charAt(start) && (end - start <= 2 || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                currentList.add(s.substring(start, end + 1));
+                dfs(result, s, end + 1, currentList, dp);
+                currentList.removeLast();
+            }
+        }
+    }
+}
+// @lc code=end
+
