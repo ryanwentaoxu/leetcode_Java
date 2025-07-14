355efd887208349f66c7e808602028427fae23aa
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-14 03:51:58 -0400

    lc 283

--- 283.move-zeroes.java
+++ 283.move-zeroes.java
@@ -7,14 +7,14 @@
 // @lc code=start
 class Solution {
     public void moveZeroes(int[] nums) {
-        int end = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                nums[end] = nums[i];
-                end += 1;
+        for (int lastNonZeroFoundAt = 0, cur = 0; cur < nums.length; cur++) {
+            if (nums[cur] != 0) {
+                int tmp = nums[cur];
+                nums[cur] = nums[lastNonZeroFoundAt];
+                nums[lastNonZeroFoundAt] = tmp;
+                lastNonZeroFoundAt += 1;
             }
         }
-        for (int i = end; i < nums.length; i++) nums[i] = 0;
     }
 }
 // @lc code=end
