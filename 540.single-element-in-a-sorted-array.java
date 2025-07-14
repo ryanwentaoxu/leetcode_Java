c9c51ce3c9f5189796284b4267d42b93548c4df9
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-13 21:56:00 -0400

    lc 540

--- 540.single-element-in-a-sorted-array.java
+++ 540.single-element-in-a-sorted-array.java
@@ -13,44 +13,36 @@ class Solution {
 
         while (left <= right) {
             int mid = (left + right) / 2;
-
-            // edge case
-            if (mid == 0 && nums[0] != nums[1]) {
-                return nums[0];
-            }
-            if (mid == 0 && nums[0] == nums[1]) {
-                left = mid + 1;
-                continue;
-            }
-
-            if (mid == nums.length - 1 && nums[mid] != nums[mid - 1]) {
-                return nums[mid];
-            }
-
-            if (mid == nums.length - 1 && nums[mid] == nums[mid - 1]) {
-                right = mid - 1;
-                continue;
-            }
-
-            // normal case
-            
-            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
-            int left_count = -1;
-            int right_count = -1;
-            if (nums[mid] == nums[mid + 1]) {
-                left_count = mid;
-                right_count = nums.length - 1 - mid;
-            }
-            
-            if (nums[mid] == nums[mid - 1]) {
-                left_count = mid - 1;
-                right_count = nums.length - mid;
-            }
-
-            if (left_count % 2 == 1) {
-                right = mid - 1;
+            if (mid == 0) {
+                if (nums[mid] != nums[mid + 1]) {
+                    return nums[mid];
+                } else {
+                    left = mid + 2;
+                }
+            } else if (mid == nums.length - 1) {
+                if (nums[mid] != nums[mid - 1]) {
+                    return nums[mid];
+                } else {
+                    right = mid - 2;
+                }
             } else {
-                left = mid + 1;
+                if (nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]) {
+                    return nums[mid];
+                } else {
+                    if (nums[mid] == nums[mid + 1]) {
+                        if (mid % 2 == 0) {
+                            left = mid + 2;
+                        } else {
+                            right = mid - 1;
+                        }
+                    } else {
+                        if ((mid - 1) % 2 == 0) {
+                            left = mid + 1;
+                        } else {
+                            right = mid - 2;
+                        }
+                    }
+                }
             }
         }
         return nums[left];
