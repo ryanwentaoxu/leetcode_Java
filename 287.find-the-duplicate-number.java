2fa85d128d49686739fb46ccc6dcec898693eb47
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-17 14:37:35 -0400

    lc 287

---
+++ 287.find-the-duplicate-number.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=287 lang=java
+ *
+ * [287] Find the Duplicate Number
+ */
+
+// @lc code=start
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int fast = 0;
+        int slow = 0;
+        do {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        } while (fast != slow);
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
+// @lc code=end
+
