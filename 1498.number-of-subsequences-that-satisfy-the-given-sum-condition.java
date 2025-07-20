df0f37c89b0eb6cc9df6559455c71cc2db76de92
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-07-20 16:41:53 -0400

    lc 1498

---
+++ 1498.number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1498 lang=java
+ *
+ * [1498] Number of Subsequences That Satisfy the Given Sum Condition
+ */
+
+// @lc code=start
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        int n = nums.length;
+        int[] power = new int[n + 1];
+        power[0] = 1;
+        int mod = 1000000007;
+        for (int i = 1; i <= n; i++) {
+            power[i] = (power[i - 1] * 2) % mod;
+        }
+        int ans = 0;
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                ans = (ans + power[right - left]) % mod;
+                left += 1;
+            } else {
+                right -= 1;
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
