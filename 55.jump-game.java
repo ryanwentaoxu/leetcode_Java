46857cfe3979a6d41254a59228b5f1b0c1ab4426
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-24 13:37:08 -0400

    lc 55

--- 55.jump-game.java
+++ 55.jump-game.java
@@ -7,14 +7,11 @@
 // @lc code=start
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
-        int lastPos = n - 1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (i + nums[i] >= lastPos) {
-                lastPos = i;
-            }
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) lastPos = i; 
         }
-        return lastPos == 0;
+        return lastPos == 0 ? true : false;
     }
 }
 // @lc code=end
