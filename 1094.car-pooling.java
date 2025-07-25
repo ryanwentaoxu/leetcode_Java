141a68d0c69cb499c7f2ff186b0aa0aaadf3c94a
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-24 23:25:31 -0400

    lc 1094

--- 1094.car-pooling.java
+++ 1094.car-pooling.java
@@ -7,28 +7,16 @@
 // @lc code=start
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int count = 0;
-        Arrays.sort(trips, (a, b) -> (a[1] - b[1]));
-        Map<Integer, Integer> map = new HashMap();
-
-        for (int i = 0; i < trips.length; i++) {
-            int num = trips[i][0];
-            int current = trips[i][1];
-            int dest = trips[i][2];
-            map.put(dest, map.getOrDefault(dest, 0) + num);
-            count += num;
-            count -= map.getOrDefault(current, 0);
-            Set<Integer> visited = new HashSet<Integer>();
-            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                if (entry.getKey() < current) {
-                    visited.add(entry.getKey());
-                    count -= entry.getValue();
-                }
-            }
-            for (int v : visited) map.remove(v);
-            map.remove(current);
-            
-            if (count > capacity) return false;
+        int maxLen = 1001;
+        int[] nums = new int[maxLen];
+        for (int[] trip : trips) {
+            nums[trip[1]] += trip[0];
+            nums[trip[2]] -= trip[0];
+        }
+        int ans = 0;
+        for (int n : nums) {
+            ans += n;
+            if (ans > capacity) return false;
         }
         return true;
     }
