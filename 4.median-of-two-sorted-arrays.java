f8bcdc2a2e9b72b1df0af07a21bf46f2ccdc8c33
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-25 11:49:46 -0400

    lc 4

--- 4.median-of-two-sorted-arrays.java
+++ 4.median-of-two-sorted-arrays.java
@@ -7,33 +7,35 @@
 // @lc code=start
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int lenA = nums1.length;
-        int lenB = nums2.length;
-        if ((lenA + lenB) % 2 == 1) {
-            return helper(nums1, nums2, (lenA + lenB) / 2, 0, lenA - 1, 0, lenB - 1);
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int n = n1 + n2;
+        if ((n1 + n2) % 2 == 0) {
+            int ans1 = helper(nums1, nums2, n / 2, 0, n1 - 1, 0, n2 - 1);
+            int ans2 = helper(nums1, nums2, n / 2 - 1, 0, n1 - 1, 0, n2 - 1);
+            return ((double)(ans1 + ans2)) / 2;
         } else {
-            int A = helper(nums1, nums2, (lenA + lenB) / 2 - 1, 0, lenA - 1, 0, lenB - 1);
-            int B = helper(nums1, nums2, (lenA + lenB) / 2, 0, lenA - 1, 0, lenB - 1);
-            return (A + B) / 2.0;
+            int ans = helper(nums1, nums2, n / 2, 0, n1 - 1, 0, n2 - 1);
+            return (double)ans;
         }
     }
 
-    public int helper(int[] A, int[] B, int k, int startA, int endA, int startB, int endB) {
-        if (startA > endA) return B[k - startA];
-        if (startB > endB) return A[k - startB];
-        int midA = (startA + endA) / 2;
-        int midB = (startB + endB) / 2;
-        if ((midA + midB) >= k) {
-            if (A[midA] > B[midB]) {
-                return helper(A, B, k, startA, midA - 1, startB, endB);
+    public int helper(int[] nums1, int[] nums2, int k, int start1, int end1, int start2, int end2) {
+        if (start1 > end1) return nums2[k - start1];
+        if (start2 > end2) return nums1[k - start2];
+        int mid1 = (start1 + end1) / 2;
+        int mid2 = (start2 + end2) / 2;
+        if ((mid1 + mid2) >= k) {
+            if (nums1[mid1] > nums2[mid2]) {
+                return helper(nums1, nums2, k, start1, mid1 - 1, start2, end2);
             } else {
-                return helper(A, B, k, startA, endA, startB, endB - 1);
+                return helper(nums1, nums2, k, start1, end1, start2, mid2 - 1);
             }
         } else {
-            if (A[midA] > B[midB]) {
-                return helper(A, B, k, startA, endA, midB + 1, endB);
+            if (nums1[mid1] > nums2[mid2]) {
+                return helper(nums1, nums2, k, start1, end1, mid2 + 1, end2);
             } else {
-                return helper(A, B, k, midA + 1, endA, startB, endB);
+                return helper(nums1, nums2, k, mid1 + 1, end1, start2, end2);
             }
         }
     }
