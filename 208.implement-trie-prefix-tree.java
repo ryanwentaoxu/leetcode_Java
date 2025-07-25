d76b0529044e2b992cc1fb3d2bb0372a6918abbf
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-25 12:54:33 -0400

    lc 208

--- 208.implement-trie-prefix-tree.java
+++ 208.implement-trie-prefix-tree.java
@@ -10,16 +10,16 @@ class TrieNode {
     int R = 26;
     boolean end = false;
 
-    public TrieNode() {
-        this.container = new TrieNode[R];
+    TrieNode(){
+        container = new TrieNode[R];
+    }
+
+    public void insert(char c) {
+        container[(int)(c - 'a')] = new TrieNode();
     }
 
     public TrieNode get(char c) {
         return container[(int)(c - 'a')];
-    }
-
-    public void put(char c) {
-        container[(int)(c - 'a')] = new TrieNode();
     }
 
     public void setEnd() {
@@ -29,11 +29,11 @@ class TrieNode {
     public boolean isEnd() {
         return this.end;
     }
+    
 }
 
 class Trie {
     TrieNode root;
-
     public Trie() {
         root = new TrieNode();
     }
@@ -41,8 +41,9 @@ class Trie {
     public void insert(String word) {
         TrieNode node = root;
         for (int i = 0; i < word.length(); i++) {
-            if (node.get(word.charAt(i)) == null) node.put(word.charAt(i));
-            node = node.get(word.charAt(i));
+            char current = word.charAt(i);
+            if (node.get(current) == null) node.insert(current);
+            node = node.get(current);
         }
         node.setEnd();
     }
@@ -50,21 +51,31 @@ class Trie {
     public boolean search(String word) {
         TrieNode node = root;
         for (int i = 0; i < word.length(); i++) {
-            if (node.get(word.charAt(i)) == null) return false;
-            node = node.get(word.charAt(i));
+            char current = word.charAt(i);
+            node = node.get(current);
+            if (node == null) return false;
         }
-        return node.isEnd() == true;
+        return node.isEnd();
     }
     
     public boolean startsWith(String prefix) {
         TrieNode node = root;
         for (int i = 0; i < prefix.length(); i++) {
-            if (node.get(prefix.charAt(i)) == null) return false;
-            node = node.get(prefix.charAt(i));
+            char current = prefix.charAt(i);
+            node = node.get(current);
+            if (node == null) return false;
         }
         return true;
     }
 }
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
 
 /**
  * Your Trie object will be instantiated and called as such:
