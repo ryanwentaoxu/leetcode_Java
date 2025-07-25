7b00249b9d1ed53e846317c6ba7eb8af2dbe4fb4
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-25 09:49:54 -0400

    lc 1498

--- 1498.number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498.number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -7,20 +7,21 @@
 // @lc code=start
 class Solution {
     public int numSubseq(int[] nums, int target) {
+        Arrays.sort(nums);
         int n = nums.length;
         int[] power = new int[n + 1];
         power[0] = 1;
-        int mod = 1000000007;
-        for (int i = 1; i <= n; i++) {
+        int mod = 1000000000 + 7;
+        for (int i = 1; i < n + 1; i++) {
             power[i] = (power[i - 1] * 2) % mod;
         }
-        int ans = 0;
-        Arrays.sort(nums);
         int left = 0;
-        int right = nums.length - 1;
+        int right = n - 1;
+        int ans = 0;
         while (left <= right) {
             if (nums[left] + nums[right] <= target) {
-                ans = (ans + power[right - left]) % mod;
+                ans += power[(right - left)];
+                ans = ans % mod;
                 left += 1;
             } else {
                 right -= 1;
