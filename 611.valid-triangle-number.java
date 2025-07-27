0285806d4ea12829f03c897c9e756209dbcca1b2
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-27 16:51:26 -0400

    lc 611

--- 611.valid-triangle-number.java
+++ 611.valid-triangle-number.java
@@ -7,16 +7,20 @@
 // @lc code=start
 class Solution {
     public int triangleNumber(int[] nums) {
-        int count = 0;
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 2; i++) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] == 0) continue;
             int k = i + 2;
-            for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
-                while (k < nums.length && nums[i] + nums[j] > nums[k]) k++;
-                count += k - j - 1;
+            for (int j = i + 1; j < n - 1; j++) {
+                while (k < n && nums[i] + nums[j] > nums[k]) {
+                    k += 1;
+                }
+                ans += k - j - 1;
             }
         }
-        return count;
+        return ans;
     }
 }
 // @lc code=end
