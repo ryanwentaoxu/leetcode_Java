424c6497eacae6b073ff1a99b4c84f64bd2f6a7d
ryanwentaoxu <wentaoxu@seas.upenn.edu>
2025-07-27 03:33:57 -0400

    lc 416

--- 416.partition-equal-subset-sum.java
+++ 416.partition-equal-subset-sum.java
@@ -6,26 +6,24 @@
 
 // @lc code=start
 class Solution {
+    Boolean[][] dp;
     public boolean canPartition(int[] nums) {
         int sum = 0;
         for (int n : nums) sum += n;
-        if (sum % 2 != 0) return false;
+        if (sum % 2 == 1) return false;
         int target = sum / 2;
-        int n = nums.length;
-        Boolean[][] memo = new Boolean[n + 1][target + 1];
-        return helper(nums, n - 1, target, memo);
+        dp = new Boolean[nums.length][target + 1];
+        return helper(nums, 0, target);
     }
 
-    public boolean helper(int[] nums, int index, int sum, Boolean[][] memo) {
-        if (sum == 0) {
-            return true;
-        }
-        if (index == 0 || sum < 0) {
-            return false;
-        }
-        if (memo[index][sum] != null) return memo[index][sum];
-        boolean result = helper(nums, index - 1, sum, memo) || helper(nums, index - 1, sum - nums[index], memo);
-        memo[index][sum] = result;
+    public boolean helper(int[] nums, int index, int target) {
+        if (target == 0) return true;
+        if (index >= nums.length) return false;
+        if (target < 0) return false;
+        if (dp[index][target] != null) return dp[index][target];
+
+        boolean result = helper(nums, index + 1, target - nums[index]) || helper(nums, index + 1, target);
+        dp[index][target] = result;
         return result;
     }
 }
