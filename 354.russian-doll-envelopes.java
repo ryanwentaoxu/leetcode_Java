472cbf14aac21fabb68987e2a0155279c1120a62
ryanwentaoxu <ryanwentaoxu@gmail.com>
2025-07-28 14:17:39 -0400

    lc 354

---
+++ 354.russian-doll-envelopes.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=354 lang=java
+ *
+ * [354] Russian Doll Envelopes
+ */
+
+// @lc code=start
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o2[1] - o1[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+        int[] secondDim = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; i++) secondDim[i] = envelopes[i][1];
+        return lengthOfLIS(secondDim);
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int len = 0;
+        for (int num : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, num);
+            if (i < 0) {
+                i = -(i + 1);
+            }
+            dp[i] = num;
+            if (i == len) len++;
+        }
+        return len;
+    }
+}
+// @lc code=end
+
